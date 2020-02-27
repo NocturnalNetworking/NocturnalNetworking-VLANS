698f36dfff19226ff8d0b2791c971439987e1966
Kelvinmocrack <56204865+Kelvinmocrack@users.noreply.github.com>
2020-02-26 21:10:50 -0500

    Update vlan.py

--- vlan.py
+++ vlan.py
@@ -1,37 +1,48 @@
 #Nocturnalnetworking.com
 #This script uses netmiko to create vlans
 from netmiko import ConnectHandler
+#This part makes the script a little bit more secure by asking the user to enter the password instead of storing it in the script.
+from getpass import getpass
+ask = getpass()
 #Dictionaries for the switches 
+
 SW1 = {
 	'device_type': 'cisco_ios',
-	'ip': '192.168.122.72',
+	'ip': '192.168.122.51',
 	'username': 'kelvin',
-	'password': 'cisco',
+	'password': ask,
 	
 }
 
 SW2 = {
 	'device_type': 'cisco_ios',
-	'ip': '192.168.122.82',
+	'ip': '192.168.122.52',
 	'username': 'kelvin',
-	'password': 'cisco',
+	'password': ask,
 }
 
 SW3 = {
 	'device_type': 'cisco_ios',
-	'ip': '192.168.122.83',
+	'ip': '192.168.122.53',
 	'username': 'kelvin',
-	'password': 'cisco',
+	'password': ask,
 }
 
 SW4 = {
 	'device_type': 'cisco_ios',
-	'ip': '192.168.122.84',
+	'ip': '192.168.122.54',
 	'username': 'kelvin',
-	'password': 'cisco',
+	'password': ask,
+}
+
+SW5 = {
+	'device_type': 'cisco_ios',
+	'ip': '192.168.122.55',
+	'username': 'kelvin',
+	'password': ask,
 }
 #A list of the dictionaries
-switches = [SW1, SW2, SW3, SW4]
+switches = [SW1, SW2, SW3, SW4,SW5]
 #Loops through the devices in the switches list and creates the Vlans.
 for devices in switches:
 	net_connect = ConnectHandler(**devices)
